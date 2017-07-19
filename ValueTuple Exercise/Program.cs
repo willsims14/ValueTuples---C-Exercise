c0565a41a8caaf0febe2b6ffaedc974a636f6b2e
William A Sims III <willsims14@gmail.com>
2017-07-19 10:38:13 -0500

    Finished

--- ValueTuple Exercise/Program.cs
+++ ValueTuple Exercise/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ValueTuple_Exercise
 {
@@ -6,7 +7,35 @@ namespace ValueTuple_Exercise
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<(string product, double amount, int quantity)> transactions = new List<(string, double, int)>();
+
+            transactions.Add(("Ball1", 45.55, 10));
+            transactions.Add(("Ball2", 40.55, 11));
+            transactions.Add(("Ball3", 41.55, 12));
+            transactions.Add(("Ball4", 43.55, 13));
+            transactions.Add(("Ball5", 42.55, 14));
+
+            Console.WriteLine("Items Sold Today:");
+            var i = 0;
+            double totalRevenue = 0;
+
+            foreach ((string product, double amount, int quantity) t in transactions)
+            {
+                // Logic goes here to look up quantity and amount in each transaction
+
+                var tempItemRevenue = t.amount * t.quantity;
+                totalRevenue += tempItemRevenue;
+                Console.WriteLine("-----" + i + "-----");
+                Console.WriteLine(" - Title: " + t.product);
+                Console.WriteLine("Revenue: " + tempItemRevenue);
+                i++;
+
+            }
+
+            Console.WriteLine("----------");
+            Console.WriteLine("----------");
+            Console.WriteLine("Total Revenue Today: $" + totalRevenue);
+            Console.ReadLine();
         }
     }
 }
